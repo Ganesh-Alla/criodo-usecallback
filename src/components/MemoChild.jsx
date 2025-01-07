0f26c8e250b5a2f601144f3fa003d7c20343127a
Ganesh Alla <allaganesh03@outlook.com>
2025-01-07 17:28:25 +0530

    Memo child update

--- src/components/MemoChild.jsx
+++ src/components/MemoChild.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 // Child Component: Receives a IncrementCount function as a prop
 const Child = ({ handleClick }) => {
-  console.log(`Increment Component rendered`);  // Log when Child is re-rendered
+  console.log(`Increment Component rendered (Child)`);  // Log when Child is re-rendered
   return (
     <div>
       <button onClick={handleClick}>Increment (Child)</button>
