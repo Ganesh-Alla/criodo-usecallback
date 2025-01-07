8b39d432767db34747b4c310c6102871a34dfdfd
Ganesh Alla <allaganesh03@outlook.com>
2025-01-07 17:27:13 +0530

    update Child3

--- src/components/Child3.jsx
+++ src/components/Child3.jsx
@@ -3,7 +3,7 @@ import React, { memo } from 'react'
 
 // Child Component: Receives a function and label as a props
 const Child = ({ increment, label }) => {
-  console.log(`${label} Component rendered`);  // Log when Child is re-rendered
+  console.log(`${label} Component rendered (child)`);  // Log when Child is re-rendered
   return (
     <div>
       <button onClick={increment}>{label} (Child)</button>
